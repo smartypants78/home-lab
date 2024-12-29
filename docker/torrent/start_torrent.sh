339d00fb7d46b62e06c2484326b7ea7a645276a6
smartypants78 <ian_t_smart@hotmail.com>
2024-12-29 19:17:07 +0000

    ...

---
+++ docker/torrent/start_torrent.sh
@@ -0,0 +1,9 @@
+docker compose -f ./gluetun/docker-compose.yml up -d
+
+echo "waiting for gluetun"
+sleep 15
+
+docker compose -f ./qbittorrent/docker-compose.yml up -d
+docker compose -f ./prowlarr/docker-compose.yml up -d
+docker compose -f ./radarr/docker-compose.yml up -d
+docker compose -f ./sonarr/docker-compose.yml up -d
---
+++ docker/torrent/stop_torrent.sh
@@ -0,0 +1,5 @@
+docker compose -f ./sonarr/docker-compose.yml down
+docker compose -f ./radarr/docker-compose.yml down
+docker compose -f ./prowlarr/docker-compose.yml down
+docker compose -f ./qbittorrent/docker-compose.yml down
+docker compose -f ./gluetun/docker-compose.yml down
