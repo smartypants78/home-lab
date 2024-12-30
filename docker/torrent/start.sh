8592f611eaeea1756eb12fd0cf99986f6d02c832
smartypants78 <ian_t_smart@hotmail.com>
2024-12-30 04:45:21 +0000

    better wait

--- docker/torrent/start.sh
+++ docker/torrent/start.sh
@@ -1,7 +1,9 @@
 docker compose -f ./gluetun/docker-compose.yml up -d
 
-echo "waiting for gluetun"
-sleep 5
+echo "waiting for gluetun to respond as healthy"
+until [ "`docker inspect -f {{.State.Health.Status}} gluetun`"=="healthy" ]; do
+    sleep 0.1;
+done;
 
 docker compose -f ./qbittorrent/docker-compose.yml up -d
 docker compose -f ./prowlarr/docker-compose.yml up -d
