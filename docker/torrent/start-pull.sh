542af0824757824ce6b68d3a3464da02e5d40e0c
smartypants78 <ian_t_smart@hotmail.com>
2025-01-11 06:45:16 +0000

    Add start script will pull first

---
+++ docker/torrent/start-pull.sh
@@ -0,0 +1,19 @@
+docker compose -f ./gluetun/docker-compose.yml pull
+docker compose -f ./gluetun/docker-compose.yml up -d
+
+echo "Waiting for gluetun to respond as healthy"
+until [ "`docker inspect -f {{.State.Health.Status}} gluetun`"=="healthy" ]; do
+    sleep 0.1;
+done;
+
+docker compose -f ./qbittorrent/docker-compose.yml pull
+docker compose -f ./qbittorrent/docker-compose.yml up -d
+
+docker compose -f ./prowlarr/docker-compose.yml pull
+docker compose -f ./prowlarr/docker-compose.yml up -d
+
+docker compose -f ./radarr/docker-compose.yml pull
+docker compose -f ./radarr/docker-compose.yml up -d
+
+docker compose -f ./sonarr/docker-compose.yml pull
+docker compose -f ./sonarr/docker-compose.yml up -d
