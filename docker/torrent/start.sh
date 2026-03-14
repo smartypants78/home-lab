fd5fd0dbd0ce50c2e568ddeda3c79f6368c31ae8
smartypants78 <ian_t_smart@hotmail.com>
2026-03-14 06:03:02 +0000

    Remove readarr

--- docker/torrent/start.sh
+++ docker/torrent/start.sh
@@ -10,5 +10,4 @@ docker compose -f ./prowlarr/docker-compose.yml up -d
 docker compose -f ./radarr/docker-compose.yml up -d
 docker compose -f ./sonarr/docker-compose.yml up -d
 docker compose -f ./lidarr/docker-compose.yml up -d
-docker compose -f ./readarr/docker-compose.yml up -d
 docker compose -f ./readmeabook/docker-compose.yml up -d
